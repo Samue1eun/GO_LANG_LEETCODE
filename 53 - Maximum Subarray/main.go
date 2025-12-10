c15c29f9499a8c1aa89c8f1243ae82a1e8a69fb5
samue1eun <samuel.eun.park@gmail.com>
2025-12-10 09:11:07 -0600

    More Leetcode Practice

---
+++ 53 - Maximum Subarray/main.go
@@ -0,0 +1,6 @@
+package main
+
+// For this problem, it is asking for the largest sum within the sub array
+// First, I will need to sum everything and find the total.
+// Minus a value from the total if the total is less than the total, return the array.
+// Utilization of Kandane's Algo
---
+++ 88 - Merge Sorted Array/main.go
@@ -0,0 +1,12 @@
+package main
+
+
+func merge(nums1 []int, m int, nums2 []int, n int)  {
+    for num in range
+}
+
+
+
+func main() {
+
+}
