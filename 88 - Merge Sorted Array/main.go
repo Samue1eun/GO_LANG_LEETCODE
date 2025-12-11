e4e3c0bfe69405d2537f407416e4875b6df3333e
samue1eun <samuel.eun.park@gmail.com>
2025-12-11 11:50:44 -0600

    Daily practice December 11

--- 88 - Merge Sorted Array/main.go
+++ 88 - Merge Sorted Array/main.go
@@ -1,12 +1,33 @@
 package main
 
+import "fmt"
 
 func merge(nums1 []int, m int, nums2 []int, n int)  {
-    for num in range
+	i := m - 1
+	j := n - 1
+	k := m + n - 1
+	// Merge from the back
+	for i >= 0 && j >= 0 {
+		if nums1[i] > nums2[j] {
+			nums1[k] = nums1[i]
+			i--
+		} else {
+			nums1[k] = nums2[j]
+			j--
+		}
+		k--
+	}
+	// If nums2 has leftovers, copy them
+	for j >= 0 {
+		nums1[k] = nums2[j]
+		j--
+		k--
+	}
+	fmt.Println(nums1)
 }
 
 
 
 func main() {
-
+	merge([]int{1, 2, 0, 0}, 2, []int{5, 6, 7, 8, 9, 10, 11, 12, 13, 11, 1, 1, 1, 1, 1, 1, 1, 1, 1,}, 2)
 }
